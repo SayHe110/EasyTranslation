5e1b704a87b78ba947e1eeff3695f7925208a297
sayhe110 <949426374@qq.com>
2018-09-26 16:59:15 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# translation
+
+百度翻译SDK
+
+## Installing
+
+```shell
+$ composer require sayhe110/translation -vvv
+```
+
+## Usage
+
+TODO
+
+## License
+
+MIT
---
+++ src/Translation.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Sayhe110\Translation;
+
+use GuzzleHttp\Client;
+
+class Translation
+{
+    protected $key;
+    protected $appid;
+    protected $guzzleOptions = [];
+
+    public function __construct($key, $appid)
+    {
+        $this->key = $key;
+        $this->appid = $appid;
+    }
+
+    public function getHttpClient()
+    {
+        return new Client($this->guzzleOptions);
+    }
+
+    public function setGuzzleOptions(array $options)
+    {
+        $this->guzzleOptions = $options;
+    }
+
+    public function translation($text, $from = 'auto', $to = 'en', $canHttps = false)
+    {
+        $url = $canHttps ?
+            'http://api.fanyi.baidu.com/api/trans/vip/translate' :
+            'https://fanyi-api.baidu.com/api/trans/vip/translate';
+
+        $salt = \random_int(10000, 99999).time();
+
+        // 根据文档生成 sign
+        $sign = md5($this->appid.$text.$salt.$this->key);
+
+        $query = array_filter([
+            'q' => $text,
+            'from' => $from,
+            'to' => $to,
+            'appid' => $this->appid,
+            'salt' => $salt,
+            'sign' => $sign,
+        ]);
+
+        $response = $this->getHttpClient()->get($url, [
+            'query' => $query,
+        ])->getBody()->getContents();
+
+        // todo 先只返回 json
+        return \json_decode($response, true);
+    }
+}
