5ffb80bf9c78f4cd24440b3ae7a5c1c6a9f37095
LeiJian <949426374@qq.com>
2019-04-29 06:56:50 +0000

    Apply fixes from StyleCI

--- src/Traits/HttpRequest.php
+++ src/Traits/HttpRequest.php
@@ -1,8 +1,16 @@
 <?php
 
+/*
+ * This file is part of the sayhe110/translation
+ *
+ * (c) sayhe110 <949426374@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Sayhe110\EasyTranslation\Traits;
 
 trait HttpRequest
 {
-
 }
